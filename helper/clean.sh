02ed99adee8f5346d1d537b9fdb22cb0c344d380
CHANGEME <CHANGEME>
2026-06-17 05:03:04 -0300

    feat: add the rest of files from the kindle

---
+++ helper/clean.sh
@@ -0,0 +1,9 @@
+
+sed 's/&nbsp;/ /g' | 
+tr '\n' '#'|
+sed 's/&n#bsp;/ /g' |
+sed 's/&#nbsp;/ /g' |
+sed 's/&nb#sp;/ /g' |
+sed 's/&nbs#p;/ /g' |
+sed 's/&nbsp#;/ /g' |
+sed 's/#/\n/g'
---
+++ helper/f.sh
@@ -0,0 +1,1 @@
+find www.thelatinlibrary.com -type f | grep -i "$1" | sed 's/www.thelatinlibrary.com/tll/g'
